4f600a6c9b29f7b527aaa95345b379975115fd12
Zackrey Bichsel <biksel09@hotmail.com>
2023-08-27 11:26:26 -0500

    debugging

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -62,6 +62,10 @@ module.exports = () => {
             },
           },
         },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
